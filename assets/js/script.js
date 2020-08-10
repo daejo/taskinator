9a5eaf0cbedcdbfa3e27d20a4126b45403179b7f
daejo <daejo@icloud.com>
2020-08-09 19:43:06 -0700

    adds form submission function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task"); //Assigns html button to a variable as "buttonEl"
+var formEl = document.querySelector("#task-form"); //Assigns html from to a variable as "formEl"
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //Assigns <ul> from html to a variable as "tasksToDoEl"
 
 var createTaskHandler = function() { //Creates the new task with a click of a button.
+    
+    event.preventDefault(); //Stops default reloading event of your browser.
+
     var listItemEl = document.createElement("li"); //Creates the element for <li>.
     listItemEl.className = "task-item"; //Says what style the created task will have.
     listItemEl.textContent = "This is a new task."; //Displays new task.
     tasksToDoEl.appendChild(listItemEl); // Method attaches a node as the last child of the list.
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler) //Links button click to createTaskHandler function.
+formEl.addEventListener("submit", createTaskHandler); //Links button click to createTaskHandler function.
